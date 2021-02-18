9b5986d221a81745bb761e3f9bfd38b6e0981bd6
aarthipriya2021 <a.aarthipriya2021@gmail.com>
2021-02-18 12:40:11 +0530

    form_project

---
+++ html/data.js
@@ -0,0 +1,82 @@
+// var dataList = 
+// {
+// 	"status":"success",
+// 	"data":[
+// 		{
+// 			"fName":"Steve",
+// 			"lName":"Jobs",
+// 			"dob": "1/1/1990",
+// 			"gender":"male",
+// 			"mobile":1234567890
+// 			"address":"zcxcvbb",
+// 			"company":"Apple.Inc.,"
+// 			"mailId":"steve@apple.com"
+// 		},
+// 		{
+// 			"fName":"Steve",
+// 			"lName":"Jobs",
+// 			"dob": "1/1/1990",
+// 			"gender":"male",
+// 			"mobile":1234567890
+// 			"address":"zcxcvbb",
+// 			"company":"Apple.Inc.,"
+// 			"mailId":"steve@apple.com"
+// 		},
+// 		{
+// 			"fName":"Steve",
+// 			"lName":"Jobs",
+// 			"dob": "1/1/1990",
+// 			"gender":"male",
+// 			"mobile":1234567890
+// 			"address":"zcxcvbb",
+// 			"company":"Apple.Inc.,"
+// 			"mailId":"steve@apple.com"
+// 		},
+// 		{
+// 			"fName":"Steve",
+// 			"lName":"Jobs",
+// 			"dob": "1/1/1990",
+// 			"gender":"male",
+// 			"mobile":1234567890
+// 			"address":"zcxcvbb",
+// 			"company":"Apple.Inc.,"
+// 			"mailId":"steve@apple.com"
+// 		}
+	
+	
+// 	]
+
+
+// }
+
+// window.onload() = loadData();
+
+// loadData () {
+
+// alert('loadData is called !!!!!!!!!!!!!')
+
+
+
+// }
+
+
+function dataList(){
+    alert("Loading........")
+}
+
+// function formAlert(){
+//     var a=document.details.fname.value;
+//     var b=document.details.lname.value;
+//     var c=document.details.gender.value;
+//     var d=document.details.dob.value;
+//     var e=document.details.mobno.value;
+//     var f=document.details.mailid.value;
+//     var g=document.details.company.value;
+//     var h=document.details.address.value;
+
+//     alert("First name :"+x+"  "+"Last name"+x+"  "+"Gender"+c+"  ");
+
+// }
+
+
+
---
+++ html/index.js
@@ -0,0 +1,49 @@
+// var name='Aarthi';// age=22;
+// var age=22;
+// console.log("Name of user's name is " + name);
+// console.log("Age of user's age is " + age);
+
+// const interestRate=0.5;
+// let interestRate=1
+// console.log(interestRate)
+// let person = {
+//     name : 'Aarthi',
+//     age : 21,
+// };
+  //Dot notation
+//person.name='Priya';
+ //Bracket notation
+//person['name']='Aarthipriya';
+//console.log(person);
+//console.log(person.age);
+  //array
+// let selectedColors = ['blue','red'];
+// selectedColors[2] = 'green';
+// console.log(selectedColors);
+
+  //function
+// function greet(name, lastname) {
+//     console.log('Hello ' + name +" " + lastname);
+// }
+
+// greet('Joe','Smith');
+// greet('Marker','black');
+
+  //function returning
+/*function square(number) {
+    console.log(number * number)
+}  
+
+square(5)*/
+
+const speak = function (name){
+  console.log('Good day ${name}');
+};
+
+speak('mary' );
+
+
+
+
+
+
